95107f1a3010e55f5b3caadc9bf382574c403589
ghani <agaarbi@gmail.com>
2022-09-08 13:04:56 +0500

    inserted data through cmd to db and returned to client

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,6 +3,6 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint, json ={"query":"Hello world!"}) # API -> Method
-print(get_response.text)
-print(get_response.status_code)
+get_response = requests.get(endpoint, json ={"product_id": 123}) # API -> Method
+print(get_response.json())
+#print(get_response.status_code)
