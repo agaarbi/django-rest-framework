5c11676f0d8ed0f0bee4e5999b58589f08754431
ghani <agaarbi@gmail.com>
2022-09-19 10:22:08 +0500

    viewsets and routers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -5,16 +5,18 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
         fields = [
+            'pk',
             'title',
             'content',
             'price',
             'sale_price',
             'my_discount',
         ]
-    
+
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
             return None
